c0b0c1d7cf94049fd96eaf46275a21d09a0c9c8f
chouette11 <keifukuda1115@gmail.com>
2022-12-17 01:20:24 +0900

    Add near location provider

--- lib/providers/presentation_providers.dart
+++ lib/providers/presentation_providers.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_template/components/appbar/appbar_notifier.dart';
 import 'package:flutter_template/pages/setting/children/notification/notification_notifier.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
 
 ///
 /// appbar
@@ -40,3 +42,21 @@ final isSettingGetupTimeProvider = StateProvider<bool>((ref) => false);
 final isNotificationProvider = StateNotifierProvider.family<IsNotificationNotifier, bool, String>(
       (ref, String key) => IsNotificationNotifier(ref, key),
 );
+
+///
+/// location
+///
+
+final nearLocationProvider = StreamProvider((ref) {
+  final db = FirebaseFirestore.instance;
+  final geo = Geoflutterfire();
+  GeoFirePoint center = geo.point(latitude: 15.960632, longitude: 77.641603);
+
+  var collectionReference = db.collection('users');
+
+  double radius = 0.1;
+  String field = 'position';
+
+  return geo.collection(collectionRef: collectionReference)
+      .within(center: center, radius: radius, field: field, strictMode: true);
+});
