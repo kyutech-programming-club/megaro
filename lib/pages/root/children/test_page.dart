100ce73d56dca4ce866dc140446ab2e0ad3612e7
light-ai <subarasiisekaiyuu.seraph@gmail.com>
2022-12-17 01:28:05 +0900

    Add GoogleMap

--- lib/pages/root/children/test_page.dart
+++ lib/pages/root/children/test_page.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_template/providers/domain_providers.dart';
 import 'package:location/location.dart';
 import '';
 import '../../../providers/location_provider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class TestPage extends ConsumerWidget {
   const TestPage({Key? key}) : super(key: key);
@@ -10,18 +14,22 @@ class TestPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
 
+    final _controller = ref.watch(googleMapProvider);
     final location = ref.watch(locationStreamProvider);
     return location.when(
       data: (loc) {
-        return Column(
-          children: [
-            Text(
-              'lat: ${loc.latitude}',
-            ),
-            Text(
-              'lon: ${loc.longitude}',
-            ),
-          ],
+        final CameraPosition _kGooglePlex = CameraPosition(
+          target: LatLng(loc.latitude!.toDouble(), loc.longitude!.toDouble()),
+          zoom: 10,
+        );
+        return GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: _kGooglePlex,
+          //markers: _markers,
+          //polylines: _lines,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
         );
       }, error: (error, stackTrace) {
       return Text(
--- lib/providers/domain_providers.dart
+++ lib/providers/domain_providers.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_template/data/preferences_data_source.dart';
 import 'package:flutter_template/data/sql/example_sql.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 final exampleDatabaseProvider = Provider<ExamplesDatabase>((ref) => ExamplesDatabase());
 
 final preferencesProvider = Provider<PreferencesDataSource>((ref) => PreferencesDataSource());
+
+final googleMapProvider = Provider<Completer>((ref) => Completer());
 
 final tokenProvider = StateProvider<String>((ref) => '');
