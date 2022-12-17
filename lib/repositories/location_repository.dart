f157f516f710a295aaa7f56a76d537d921f90456
chouette11 <keifukuda1115@gmail.com>
2022-12-17 09:49:49 +0900

    Add location stream to location repository

--- lib/repositories/location_repository.dart
+++ lib/repositories/location_repository.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_template/data/firestore_data_source.dart';
 import 'package:flutter_template/entity/location/location_entity.dart';
+import 'package:location/location.dart';
 
 import '../documents/location_document/location_document.dart';
 
@@ -15,15 +16,24 @@ class LocationRepository {
   /// 投稿新規追加
   Future<void> addLocation(LocationEntity locationEntity) async {
     final firestore = ref.read(firestoreProvider);
-    final locationDoc = await LocationDocument.createDocument(locationEntity, ref);
+    final locationDoc =
+        await LocationDocument.createDocument(locationEntity, ref);
     await firestore.insertLocation(locationDoc);
   }
 
-  /// タスクのストリームを取得
-  // Stream<List<LocationEntity>> getLocationStream() {
-  //   final sql = ref.read(sqlProvider);
-  //   return sql.getLocationStream().map((event) => event
-  //       .map((e) => ConvertUtils.instance.toLocationFromSql(locationSqlDocument: e))
-  //       .toList());
-  // }
+  /// 近くのストリームを取得
+  Stream<List<LocationEntity>> getNearLocationStream(LocationData locData) {
+    final firestore = ref.read(firestoreProvider);
+    return firestore.fetchNearLocation(locData).map(
+          (event) => event.map((e) => LocationEntity.fromDoc(e)).toList(),
+        );
+  }
+
+  /// 現在地のストリームを取得
+  Stream<LocationData> getCurrentLocationStream() {
+    final location = Location();
+    // ignore: cascade_invocations
+    // location.enableBackgroundMode(enable: true);
+    return location.onLocationChanged;
+  }
 }
