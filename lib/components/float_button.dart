9e2112c7a404e2a72d810f747a0f138fc77c27fb
chocolate-kuma <natsui0814okada@gmail.com>
2022-12-17 12:38:35 +0900

    change FloatingActionButton

--- lib/components/float_button.dart
+++ lib/components/float_button.dart
@@ -1,39 +1,24 @@
-import 'package:flutter/material.dart' as float_button;
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_template/components/modal/modal.dart';
 import 'package:flutter_template/constants/color_constant.dart';
-import 'package:flutter_template/providers/presentation_providers.dart';
+import 'package:go_router/go_router.dart';
 
-class FloatButton extends ConsumerWidget {
-  const FloatButton({float_button.Key? key}) : super(key: key);
-
+class MessageButton extends ConsumerWidget {
+  const MessageButton({Key? key}) : super(key: key);
   @override
-  float_button.Widget build(float_button.BuildContext context, WidgetRef ref) {
-       return float_button.FloatingActionButton(
-            onPressed: () {
-              float_button.showModalBottomSheet(
-                  shape: float_button.RoundedRectangleBorder(
-                    borderRadius: float_button.BorderRadius.vertical(top: float_button.Radius.circular(8)),
-                  ),
-                  isScrollControlled: true,
-                  context: context,
-                  builder: (float_button.BuildContext context) {
-                    return float_button.Padding(
-                        padding: float_button.EdgeInsets.only(
-                        bottom: float_button.MediaQuery.of(context).viewInsets.bottom,
-                        ),
-                      child: Modal(),
-                    );
-                  });
-              ref
-                  .watch(isAppbarTextFormField.notifier)
-                  .update((state) => !state);
-            },
-            backgroundColor: ColorConstant.purple40,
-            child: float_button.Icon(
-              float_button.Icons.add,
-              color: ColorConstant.black100,
+  Widget build(BuildContext context, WidgetRef ref) {
+       return SizedBox(
+         width: 64,
+         height: 64,
+         child: FloatingActionButton(
+              onPressed: () => context.push('/message_page/:'),
+              backgroundColor: ColorConstant.purple40,
+              child: Icon(
+
+                Icons.chat_bubble,
+                color: ColorConstant.black100,
+              ),
             ),
-          );
+       );
   }
 }
