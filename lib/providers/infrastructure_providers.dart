3e62fbdc974ba4f6ef0352177f15544c657f1aec
chouette11 <keifukuda1115@gmail.com>
2022-12-17 09:47:16 +0900

    Add location providers to interface

--- lib/providers/infrastructure_providers.dart
+++ lib/providers/infrastructure_providers.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_template/entity/example/example_entity.dart';
+import 'package:flutter_template/entity/location/location_entity.dart';
 import 'package:flutter_template/repositories/example_repository.dart';
+import 'package:flutter_template/repositories/location_repository.dart';
+import 'package:location/location.dart';
 
 /// Firestoreのインスタンスを保持するプロバイダ
 final firebaseFirestoreProvider = Provider((_) => FirebaseFirestore.instance);
@@ -15,7 +18,21 @@ final examplesCollectionRefProvider =
         .watch(firebaseFirestoreProvider)
         .collection(ref.watch(examplesCollectionNameProvider)));
 
-/// StreamProvider
+///
+/// Location
+///
+
+/// 近くの位置情報
+final nearLocationsStreamProvider =
+    StreamProvider.family<List<LocationEntity>, LocationData>((ref, locData) {
+  return ref.read(locationRepositoryProvider).getNearLocationStream(locData);
+});
+
+/// 現在地
+final currentLocationStreamProvider = StreamProvider<LocationData>((ref) {
+  return ref.read(locationRepositoryProvider).getCurrentLocationStream();
+});
+
 final StreamProvider<List<ExampleEntity>> examplesStreamProvider =
     StreamProvider<List<ExampleEntity>>((ref) {
   return ref.read(exampleRepositoryProvider).getExampleStream();
